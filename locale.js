53a545acc3eab8084eafea80940ff2fb516927d3
bit0r1n <smth@altir.xyz>
2020-07-30 10:10:29 +0300

    оп ахах

--- locale.js
+++ locale.js
@@ -4,7 +4,7 @@ const { writeFileSync } = require('fs');
 const locales = ['ru'];
 (async () => {
 	for (const lang of locales) {
-		const { game_stats } = await api.getLocales(lang, 'game_stats'); // если это читают разработчики API VimeWorld, то сделай ПЖ цветовые ассоциации к ранкам в локализации, ну или где-либо ещё, спасибо :))))))))))))
+		const { game_stats } = await api.getLocales(lang, 'game_stats'); // если это читают разработчики API VimeWorld, то сделайте ПЖ цветовые ассоциации к ранкам в локализации, ну или где-либо ещё, спасибо :))))))))))))
 		writeFileSync(`i18n/${lang}/games.json`, JSON.stringify(game_stats));
 	}
 })();
